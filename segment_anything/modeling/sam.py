3bd035752967e6c98d1b0c6c937fe80edaf3b9d7
Benjamin Olsen <thelarrys@gmail.com>
2023-08-24 16:07:39 +0200

    add Optional

--- segment_anything/modeling/sam.py
+++ segment_anything/modeling/sam.py
@@ -8,7 +8,7 @@ import torch
 from torch import nn
 from torch.nn import functional as F
 
-from typing import Any, Dict, List, Tuple
+from typing import Any, Dict, List, Tuple, Optional
 
 from .image_encoder import ImageEncoderViT
 from .mask_decoder import MaskDecoder
